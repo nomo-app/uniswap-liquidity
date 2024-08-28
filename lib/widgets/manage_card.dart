57356424af5dcbf6c0d6db5e9b880a0cca96bb7b
dev2-nomo <dev2@nomo.app>
2024-08-28 18:24:58 +0200

    fix lint

--- lib/widgets/manage_card.dart
+++ lib/widgets/manage_card.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nomo_ui_kit/components/card/nomo_card.dart';
-import 'package:nomo_ui_kit/theme/nomo_theme.dart';
 import 'package:nomo_ui_kit/utils/layout_extensions.dart';
 import 'package:uniswap_liquidity/widgets/add_liquidity_box.dart';
 import 'package:uniswap_liquidity/widgets/manage_buttons.dart';
