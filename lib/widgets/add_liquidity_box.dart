b8eea18d247916ba27b3ec65bd606d3d7c5d87d8
dev2-nomo <dev2@nomo.app>
2024-08-28 17:55:04 +0200

    add token image to input

--- lib/widgets/add_liquidity_box.dart
+++ lib/widgets/add_liquidity_box.dart
@@ -25,7 +25,7 @@ class AddLiquidityBox extends HookConsumerWidget {
           balance: zeniqBalance,
           errorNotifier: formStateNotifier.zeniqErrorNotifier,
           valueNotifier: formStateNotifier.zeniqNotifier,
-          fiatBlance: pool.fiatZeniqBalance!,
+          fiatBlance: pool.fiatZeniqBalance,
         ),
         12.vSpacing,
         Icon(
@@ -39,7 +39,7 @@ class AddLiquidityBox extends HookConsumerWidget {
           balance: pool.balanceToken ?? Amount.zero,
           errorNotifier: formStateNotifier.tokenErrorNotifier,
           valueNotifier: formStateNotifier.tokenNotifier,
-          fiatBlance: pool.fiatBlanceToken!,
+          fiatBlance: pool.fiatBlanceToken,
         ),
       ],
     );
--- lib/widgets/liquidity_input_field.dart
+++ lib/widgets/liquidity_input_field.dart
@@ -5,14 +5,15 @@ import 'package:nomo_ui_kit/components/input/textInput/nomo_input.dart';
 import 'package:nomo_ui_kit/components/text/nomo_text.dart';
 import 'package:nomo_ui_kit/theme/nomo_theme.dart';
 import 'package:nomo_ui_kit/utils/layout_extensions.dart';
+import 'package:uniswap_liquidity/provider/asset_provider.dart';
 import 'package:walletkit_dart/walletkit_dart.dart';
 
 class LiquidityInputField extends HookConsumerWidget {
   final EthBasedTokenEntity? token;
-  final Amount balance;
+  final Amount? balance;
   final ValueNotifier<String?> errorNotifier;
   final ValueNotifier<String> valueNotifier;
-  final double fiatBlance;
+  final double? fiatBlance;
   const LiquidityInputField({
     super.key,
     required this.token,
@@ -23,85 +24,104 @@ class LiquidityInputField extends HookConsumerWidget {
   });
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: context.theme.colors.onDisabled,
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          NomoInput(
-            errorBorder: Border.fromBorderSide(
-              BorderSide(color: context.theme.colors.error),
+    final tokenImage =
+        ref.read(assetNotifierProvider).imageNotifierForToken(token!)!;
+
+    return ListenableBuilder(
+        listenable: tokenImage,
+        builder: (context, child) {
+          final image = tokenImage.value;
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: context.theme.colors.onDisabled,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(8),
             ),
-            borderRadius: BorderRadius.circular(8),
-            errorNotifier: errorNotifier,
-            errorColor: Colors.red,
-            valueNotifier: valueNotifier,
-            maxLines: 1,
-            keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            selectedBorder: Border.fromBorderSide(
-              BorderSide(color: context.theme.colors.surface),
-            ),
-            inputFormatters: [
-              CustomNumberInputFormatter(decimals: 7),
-            ],
-            background: context.theme.colors.surface,
-            placeHolder: "0.0",
-            placeHolderStyle: context.typography.b3,
-            style: context.typography.b3,
-            trailling: Row(
+            child: Column(
               mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  height: 30,
-                  width: 30,
-                  color: Colors.red,
+                NomoInput(
+                  errorBorder: Border.fromBorderSide(
+                    BorderSide(color: context.theme.colors.error),
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                  errorNotifier: errorNotifier,
+                  errorColor: Colors.red,
+                  valueNotifier: valueNotifier,
+                  maxLines: 1,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  selectedBorder: Border.fromBorderSide(
+                    BorderSide(color: context.theme.colors.surface),
+                  ),
+                  inputFormatters: [
+                    CustomNumberInputFormatter(decimals: 7),
+                  ],
+                  background: context.theme.colors.surface,
+                  placeHolder: "0.0",
+                  placeHolderStyle: context.typography.b3,
+                  style: context.typography.b3,
+                  trailling: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      image.when(
+                        data: (data) => ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            data.small,
+                            width: 34,
+                            height: 34,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        error: (error, stackTrace) => Text(
+                          error.toString(),
+                        ),
+                        loading: () => CircularProgressIndicator(),
+                      ),
+                      8.hSpacing,
+                      NomoText(
+                        token?.symbol ?? "nav",
+                        style: context.typography.b2,
+                      ),
+                    ],
+                  ),
+                  height: 58,
                 ),
-                8.hSpacing,
-                NomoText(
-                  token?.symbol ?? "nav",
-                  style: context.typography.b2,
+                8.vSpacing,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    bottom: 12,
+                    right: 16,
+                  ),
+                  child: Row(
+                    children: [
+                      NomoText(
+                        "\$${fiatBlance?.toStringAsFixed(2) ?? "0.00"}",
+                        style: context.typography.b1,
+                      ),
+                      const Spacer(),
+                      Icon(
+                        Icons.wallet,
+                        color: context.theme.colors.foreground1,
+                      ),
+                      8.hSpacing,
+                      NomoText(
+                        balance?.displayValue ?? "0.00",
+                        style: context.typography.b1,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-            height: 58,
-          ),
-          8.vSpacing,
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16,
-              bottom: 12,
-              right: 16,
-            ),
-            child: Row(
-              children: [
-                NomoText(
-                  "\$${fiatBlance.toStringAsFixed(2)}",
-                  style: context.typography.b1,
-                ),
-                const Spacer(),
-                Icon(
-                  Icons.wallet,
-                  color: context.theme.colors.foreground1,
-                ),
-                8.hSpacing,
-                NomoText(
-                  balance.displayValue,
-                  style: context.typography.b1,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
 
