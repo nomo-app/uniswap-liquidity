24d81d6547f39e753aa78c145fa2d51a9a106ce3
dev2-nomo <dev2@nomo.app>
2024-09-25 16:56:19 +0200

    copy web screen from uniswap webon

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nomo_router/nomo_router.dart';
 import 'package:nomo_ui_kit/app/nomo_app.dart';
 import 'package:nomo_ui_kit/components/buttons/primary/nomo_primary_button.dart';
+import 'package:nomo_ui_kit/components/card/nomo_card.dart';
 import 'package:nomo_ui_kit/components/text/nomo_text.dart';
+import 'package:nomo_ui_kit/theme/nomo_theme.dart';
 import 'package:uniswap_liquidity/routes.dart';
 import 'package:uniswap_liquidity/theme.dart';
 import 'package:uniswap_liquidity/utils/rpc.dart';
@@ -16,6 +20,7 @@ import 'package:webon_kit_dart/webon_kit_dart.dart';
 final appRouter = AppRouter();
 var address = "";
 Amount zeniqBalance = Amount.zero;
+const deeplink = 'https://nomo.app/webon/liquidity.zeniqswap.com';
 
 void main() async {
   usePathUrlStrategy();
@@ -29,56 +34,142 @@ void main() async {
     zeniqBalance = await rpc.fetchTokenBalance(address, zeniqETHToken);
     print('EVM address: $address');
   } catch (e) {
+    final textStyle = GoogleFonts.roboto(
+      color: Colors.white,
+      fontSize: 16,
+    );
     print(e);
     runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         home: NomoDefaultTextStyle(
           style: const TextStyle(color: Colors.white, fontSize: 32),
-          child: Scaffold(
-            backgroundColor: Colors.black87,
-            body: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(
-                      Icons.warning,
-                      size: 96.0,
-                      color: Colors.redAccent,
-                    ),
-                    const SizedBox(height: 24.0),
-                    const NomoText(
-                      'Not inside the NomoApp. Please use zeniqswap.com for providing liquidity in the browser.',
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 16.0),
-                    const NomoText(
-                      'Or download the Nomo App from the App Store or Google Play Store.',
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 64.0),
-                    PrimaryNomoButton(
-                      text: "Download Nomo App",
-                      textStyle: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
+          child: Builder(builder: (context) {
+            return Scaffold(
+              backgroundColor: Colors.black,
+              extendBodyBehindAppBar: true,
+              body: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 32,
+                        ),
+                        child: NomoCard(
+                          backgroundColor: Color(0xff1e2428).withOpacity(0.5),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 24,
+                            vertical: 8,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Image.asset(
+                                'assets/logo.png',
+                                width: 28,
+                                height: 28,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(
+                                'Zeniq Pools',
+                                style: GoogleFonts.dancingScript(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                            ],
+                          ),
+                        ),
                       ),
-                      elevation: 10,
-                      height: 64,
-                      borderRadius: BorderRadius.circular(12),
-                      padding: EdgeInsets.zero,
-                      width: 320,
-                      onPressed: () async {
-                        await launchUrlString("https://nomo.app/install");
-                      },
-                    )
-                  ],
-                ),
+                      Spacer(),
+                      Center(
+                        child: NomoCard(
+                          backgroundColor: Color(0xff1e2428).withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(12),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24, vertical: 48),
+                          child: SizedBox(
+                            width: 380,
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Coming Soon",
+                                  style: GoogleFonts.roboto().copyWith(
+                                    fontSize: 36,
+                                    // fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 32.0),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: Colors.white,
+                                  ),
+                                  padding: EdgeInsets.all(16),
+                                  width: 200,
+                                  height: 200,
+                                  child: GestureDetector(
+                                    onTap: () async {
+                                      await launchUrlString(
+                                        'https://nomo.app/webon/dex.zeniqswap.com',
+                                      );
+                                    },
+                                    child: BarcodeWidget(
+                                      data: deeplink,
+                                      color: Colors.black,
+                                      barcode: Barcode.fromType(
+                                        BarcodeType.QrCode,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 32.0),
+                                Text(
+                                  'Not inside the NomoApp. Please use zeniqswap.com for providing liquidity in the browser.',
+                                  textAlign: TextAlign.center,
+                                  style: textStyle,
+                                ),
+                                const SizedBox(height: 16.0),
+                                Text(
+                                  'Or download the Nomo App from the App Store or Google Play Store.',
+                                  textAlign: TextAlign.center,
+                                  style: textStyle,
+                                ),
+                                const SizedBox(height: 32.0),
+                                PrimaryNomoButton(
+                                  text: "Download Nomo App",
+                                  textStyle: GoogleFonts.roboto(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                  ),
+                                  elevation: 0,
+                                  height: 64,
+                                  backgroundColor: primaryColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                  padding: EdgeInsets.zero,
+                                  width: 320,
+                                  onPressed: () async {
+                                    await launchUrlString(deeplink);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                ],
               ),
-            ),
-          ),
+            );
+          }),
         ),
       ),
     );
